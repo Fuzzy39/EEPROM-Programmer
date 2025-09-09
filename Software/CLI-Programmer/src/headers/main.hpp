3b77e1d80490ce0f02708822344f84e2f55fb677
Fuzzy39 <mshill126@gmail.com>
2025-09-09 13:06:52 -0400

    Some semi-working usb code

--- Software/CLI-Programmer/src/headers/main.hpp
+++ Software/CLI-Programmer/src/headers/main.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-int x = 5;
+// nothing here...
--- Software/CLI-Programmer/src/main.cpp
+++ Software/CLI-Programmer/src/main.cpp
@@ -1,9 +1,65 @@
 #include <iostream>
 #include "main.hpp"
+#include <libusb-1.0/libusb.h>
 
 int main(void)
 {
-    std::cout<<"Hello!\n";
-    std::cout<<x<<"\n";
+    int err;
+
+    libusb_init_option options[] =  {{LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_INFO}};
+
+    if(libusb_init_context(nullptr, options,1))
+    {
+        std::cout<<"Failed to Initialize.\n";
+        return 1;
+    }
+    
+    // try to enumerate devices
+    libusb_device **list;
+
+    ssize_t count = libusb_get_device_list(nullptr, &list);
+    std::cout<<"Found "<< count <<" devices.\n";
+
+    for(int i = 0; i<count; i++)
+    {
+        libusb_device *device = list[i];
+        // the goal here is to get the name of the product string..
+        //std::cout<<"Getting desc.\n";
+        libusb_device_descriptor descriptor;
+        if(err = libusb_get_device_descriptor(device, &descriptor))
+        {
+            std::cout<<"ERROR: "<<libusb_error_name(err)<<"\n";
+            return err;
+        }
+        
+        // we have to open the device to get a string from it.
+        libusb_device_handle* handle;
+        if(err = libusb_open(device, &handle))
+        {
+            std::cout<<"ERROR: "<<"Unknown device..."<<"\n";
+            continue;
+        }
+
+        //std::cout<<"Getting name...\n";
+        unsigned char productStr[1000] ;
+        if(err = libusb_get_string_descriptor_ascii(handle, descriptor.idProduct, productStr, 1000))
+        {
+            std::cout<<"ERROR: "<<libusb_error_name(err)<<"\n";
+            //return err;
+            //continue;
+        }
+
+        libusb_close(handle);
+
+        std::cout<<productStr<<"\n";
+    }
+    
+
+
+    // we have to discard the list right 
+    libusb_free_device_list(list, true);
+
+    libusb_exit(nullptr);
+    std::cout<<"Sucess, maybe!\n";
     return 0;
 }
